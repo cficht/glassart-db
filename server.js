34f44d0e6a3e5097e8f3964e405f48aae716017e
cficht <chris.ficht@gmail.com>
2020-02-21 09:36:30 -0800

    made server.js

--- server.js
+++ server.js
@@ -3,26 +3,56 @@ require('dotenv').config();
 
 // Application Dependencies
 const express = require('express');
-// (add cors, pg, and morgan...)
+const cors = require('cors');
+const morgan = require('morgan');
+const pg = require('pg');
 
+console.log(process.env);
 // Database Client
-// (create and connect using DATABASE_URL)
+const Client = pg.Client;
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
 
 
 // Application Setup
 const app = express();
-// (add middleware utils: logging, cors, static files from public)
-// app.use(...)
-
+const PORT = process.env.PORT;
+app.use(morgan('dev'));
+app.use(cors());
 
 // API Routes
+
+app.get('/api/data', async (req, res) => {
+    try {
+        const result = await client.query(`
+        SELECT
+        product_id,
+        product_name,
+        description,
+        price,
+        img_url,
+        type,
+        in_stock,
+        quantity
+        FROM glass_art;
+        `);
+
+        console.log(result.rows);
+
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
 
 // http method and path...
 
 
 // Start the server
 // (use PORT from .env!)
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log('server running on PORT', PORT);
