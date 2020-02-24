da1be1074f0c17932e5f1e6efada9855f45e601c
cficht <chris.ficht@gmail.com>
2020-02-24 09:33:55 -0800

    updated get route to join tables, created get routes for types and single resource, created post route for inserting item

--- server.js
+++ server.js
@@ -20,22 +20,72 @@ const PORT = process.env.PORT;
 app.use(morgan('dev'));
 app.use(cors());
 app.use('/assets', express.static('public')); //allows us to host images folder
-
+app.use(express.json()); // enable reading incoming json data
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 // API Routes
 
 app.get('/api/data', async (req, res) => {
     try {
         const result = await client.query(`
-        SELECT
-        product_id,
-        product_name,
-        description,
-        price,
-        img_url,
-        type,
-        in_stock,
-        quantity
-        FROM glass_art;
+            SELECT
+                glass_art.*
+            FROM glass_art
+            JOIN glass_type
+            ON glass_art.type_id = glass_type.type_id
+            ORDER BY glass_art.product_id;
+        `);
+
+        console.log(result.rows);
+
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.post('/api/data', async (req, res) => {
+    try {
+        console.log(req.body);
+        const result = await client.query(`
+        INSERT INTO glass_art (product_name, description, price, img_url, type_id, in_stock, quantity)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING *;
+        `,
+
+        [req.body.product_name, req.body.description, req.body.price, req.body.img_url, req.body.type_id, req.body.in_stock, req.body.quantity]
+        );
+        res.json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.get('/api/data/:myGlassId/', async (req, res) => {
+    try {
+        const result = await client.query(`
+            SELECT *
+            FROM glass_art
+            WHERE glass_art.product_id=$1`,
+        [req.params.myGlassId]);
+
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.get('/api/types', async (req, res) => {
+    try {
+        const result = await client.query(`
+            SELECT *
+            FROM glass_type
+            ORDER BY type_id;
         `);
 
         console.log(result.rows);
